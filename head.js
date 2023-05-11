8b8227eaa86bbc250da465788f73b228062d83e8
Wojciech Kropidlowski <w.kropidlowski@accenture.com>
2023-05-11 14:55:12 +0200

    CDRW-4197 - update footer

--- head.js
+++
@@ -1,8 +0,0 @@
-// // https://vuepress.vuejs.org/config/#head
-
-module.exports = [
-    [
-        ['link', { rel: 'icon', href: '/assets/img/favicon.ico' }],
-        ['link', { rel: 'manifest', href: '/manifest.json' }],
-    ],
-];
